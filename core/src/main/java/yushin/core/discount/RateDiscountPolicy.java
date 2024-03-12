1b2ad4657dc45cef3655a2ae97cf87ad61c26388
Yush1nk1m <kys010306@sogang.ac.kr>
2024-03-12 09:23:56 +0900

    20240312

---
+++ core/src/main/java/yushin/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,2 @@
+package yushin.core.discount;public class RateDiscountPolicy {
+}
---
+++ core/src/test/java/yushin/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,6 @@
+package yushin.core.discount;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RateDiscountPolicyTest {
+  
+}
