23035ad97bdf18d3ac1adb8051da6bb7fb6536d7
Yush1nk1m <kys010306@sogang.ac.kr>
2024-03-19 23:48:24 +0900

    20240319

--- core/src/main/java/yushin/core/web/LogDemoController.java
+++ core/src/main/java/yushin/core/web/LogDemoController.java
@@ -1,2 +1,30 @@
-package yushin.core.web;public class LogDemoController {
+package yushin.core.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import yushin.core.common.MyLogger;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        System.out.println("myLogger = " + myLogger.getClass());
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
 }
--- core/src/main/java/yushin/core/web/LogDemoService.java
+++ core/src/main/java/yushin/core/web/LogDemoService.java
@@ -1,2 +1,17 @@
-package yushin.core.web;public class LogDemoService {
+package yushin.core.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+import yushin.core.common.MyLogger;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
 }
