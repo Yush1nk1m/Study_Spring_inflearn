9b59f1279870434b948a41bdb52782caef3b4d6b
Yush1nk1m <kys010306@sogang.ac.kr>
2024-03-27 23:09:33 +0900

    20240327

---
+++ servlet/src/main/java/yushin/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,27 @@
+package yushin.servlet.web.frontcontroller.v2.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import yushin.servlet.domain.member.Member;
+import yushin.servlet.domain.member.MemberRepository;
+import yushin.servlet.web.frontcontroller.MyView;
+import yushin.servlet.web.frontcontroller.v2.ControllerV2;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
+}
