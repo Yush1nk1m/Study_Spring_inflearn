70513c99fd248372778a377318246ddab682f4fd
Yush1nk1m <kys010306@sogang.ac.kr>
2024-03-30 00:41:37 +0900

    20240330

---
+++ springmvc/src/test/java/yushin/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package yushin.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
