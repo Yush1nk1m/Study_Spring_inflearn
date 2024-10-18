6c46b29f7fcb81c80060d19e42678f121d55965a
Yush1nk1m <kys010306@sogang.ac.kr>
2024-10-19 02:09:57 +0900

    20241019: ~ 김영한 스프링 DB 2편, 섹션 3

--- projects/itemservice-db/src/main/java/hello/itemservice/config/MemoryConfig.java
+++ projects/itemservice-db/src/main/java/hello/itemservice/config/MemoryConfig.java
@@ -1,20 +1,14 @@
 package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
-import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV1;
+import hello.itemservice.repository.memory.MemoryItemRepository;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
-@RequiredArgsConstructor
-public class JdbcTemplateV1Config {
-
-    private final DataSource dataSource;
+public class MemoryConfig {
 
     @Bean
     public ItemService itemService() {
@@ -23,6 +17,7 @@ public class JdbcTemplateV1Config {
 
     @Bean
     public ItemRepository itemRepository() {
-        return new JdbcTemplateItemRepositoryV1(dataSource);
+        return new MemoryItemRepository();
     }
+
 }
