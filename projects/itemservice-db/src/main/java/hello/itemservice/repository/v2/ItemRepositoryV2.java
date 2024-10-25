b96a3f0a8f129a614e8506dddfb9b9685dca3e91
Yush1nk1m <kys010306@sogang.ac.kr>
2024-10-25 20:06:10 +0900

    20241025: 김영한 스프링 DB 2편 완강

--- projects/itemservice-db/src/main/java/hello/itemservice/repository/v2/ItemRepositoryV2.java
+++ projects/itemservice-db/src/main/java/hello/itemservice/repository/v2/ItemRepositoryV2.java
@@ -1,4 +1,7 @@
 package hello.itemservice.repository.v2;
 
-public interface ItemRepositoryV2 {
+import hello.itemservice.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepositoryV2 extends JpaRepository<Item, Long> {
 }
--- projects/springtx/src/main/java/hello/springtx/order/NotEnoughMoneyException.java
+++ projects/springtx/src/main/java/hello/springtx/order/NotEnoughMoneyException.java
@@ -1,4 +1,8 @@
 package hello.springtx.order;
 
-public class NotEnoughMoneyException {
+public class NotEnoughMoneyException extends Exception {
+
+    public NotEnoughMoneyException(String message) {
+        super(message);
+    }
 }
