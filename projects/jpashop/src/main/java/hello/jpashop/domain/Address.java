758193a5bdc579640e877264fc79d95e4e1f9cc5
Yush1nk1m <kys010306@sogang.ac.kr>
2024-10-28 20:50:17 +0900

    20241027: 김영한 스프링 JPA 섹션 6, 실전 예제 6

---
+++ projects/jpashop/src/main/java/hello/jpashop/domain/Address.java
@@ -0,0 +1,16 @@
+package hello.jpashop.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+@EqualsAndHashCode
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+}
