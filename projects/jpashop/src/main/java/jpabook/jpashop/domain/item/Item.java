67ee2db946ad4a0813015e0ef01471022fcc3fee
Yush1nk1m <kys010306@sogang.ac.kr>
2025-12-28 15:00:08 +0900

    20251228: 김영한 실전 스프링 부트 1
    - 섹션 6 정리

--- projects/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ projects/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.*;
 
 import java.util.*;
@@ -23,5 +24,21 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // == 비즈니스 로직 == //
+
+    // stock 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    // stock 감소
+    public void removeStock(int quantity) {
+        int leftStock = this.stockQuantity - quantity;
+        if (leftStock < 0) {
+            throw new NotEnoughStockException("Not enough stock");
+        }
+        this.stockQuantity = leftStock;
+    }
 }
 
